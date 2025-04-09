7e1ee6a8cec5f8c376379252fbaad4bdcc6e9fc3
alexrilo <alejandroriloferreiro1@gmail.com>
2025-04-09 18:00:50 +0200

    Añadido contacto al navbar + bloquear scroll cuando se abre navbar y desbloquear cuando se cierra

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// Selecciona todos los enlaces del navbar
-const navbarLinks = document.querySelectorAll('.navbar a');
-const menuToggle = document.getElementById('menu-toggle');
 
-// Agrega un evento de clic a cada enlace
-navbarLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        // Desmarca el checkbox para cerrar el menú
-        menuToggle.checked = false;
+    // Selecciona todos los enlaces del navbar
+    const navbarLinks = document.querySelectorAll('.navbar a');
+    const menuToggle = document.getElementById('menu-toggle');
+
+    // Agrega un evento de clic a cada enlace
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            // Desmarca el checkbox para cerrar el menú
+            menuToggle.checked = false;
+            document.body.classList.remove('no-scroll');
+        });
     });
-});
+
+    menuToggle.addEventListener('change', () => {
+        document.body.classList.toggle('no-scroll', menuToggle.checked);
+    });
